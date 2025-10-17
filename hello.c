46903e5251b18d2b7ea92d9d9b8419b032e74f86
t236040-bit <t236040@st.yamagata-u.ac.jp>
2025-10-17 18:33:22 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hi,who are you.");
+    
+    return 0;
+}
---
+++ number.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int gozzila = 666;
+    printf("Gozzila number %d", gozzila);
+    
+    return 0;
+}
